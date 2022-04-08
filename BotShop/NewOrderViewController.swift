f860d1178718a4a819491aff50d3201eda8a6c53
Jose <hose153@gmail.com>
2022-04-08 18:18:09 -0500

    Users can navigate between pages using tabbar

---
+++ BotShop/NewOrderViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  BotShop
+//
+//  Created by Jose Salazar Mendez on 4/8/22.
+//
+
+import UIKit
+
+class NewOrderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+}
--- BotShop/PastOrderViewController.swift
+++ BotShop/PastOrderViewController.swift
@@ -1,18 +1,18 @@
 //
-//  ViewController.swift
+//  PastOrderViewController.swift
 //  BotShop
 //
 //  Created by Jose Salazar Mendez on 4/8/22.
 //
-
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
+class PastOrderViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.backgroundColor = .red
+        view.backgroundColor = .green
     }
 
 }
