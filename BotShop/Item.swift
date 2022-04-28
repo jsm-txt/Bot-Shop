824628cfbc0881ca763903d2c23167143dcb6cd6
Jose <hose153@gmail.com>
2022-04-28 18:36:41 -0500

    Users can see items for sale

---
+++ BotShop/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  BotShop
+//
+//  Created by Jose Salazar Mendez on 4/28/22.
+//
+
+import Foundation
+import UIKit
+
+struct Item {
+    var title: String
+    var image: UIImage
+}
